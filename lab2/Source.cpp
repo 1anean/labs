f101d38370671e56b77a595f3340f8c82cb4f070
1anean <vectorina0@gmail.com>
2023-02-17 21:23:16 +0300

    first commit

---
+++ lab2/Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	//lab2
+
+	/*string surname;
+	float age;
+	int course;
+
+	cout << "введите фамилию: ";
+	cin >> surname;
+	cout << "введите возраст: ";
+	cin >> age;
+	cout << "введите номер курса: ";
+	cin >> course;
+	cout << "\nваши данные:\nфамилия: " << surname << " \nвозраст: " << age << " \nномер курса: " << course << "\n";*/
+
+	//lab2.1
+
+	return 0;
+
+}
