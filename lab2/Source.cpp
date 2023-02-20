9cc9db552f52e6530a3a862d7483562009e055e7
1anean <vectorina0@gmail.com>
2023-02-20 12:42:08 +0300

    changes

--- lab2/Source.cpp
+++ lab2/Source.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <studio.h>
+#include <windows.h>
 
 
 using namespace std;
@@ -12,19 +12,67 @@ int main()
 
 	//lab2
 
+
 	/*string surname;
 	float age;
 	int course;
 
 	cout << "введите фамилию: ";
 	cin >> surname;
-	cout << "введите возраст: ";
+	cout << "введите возраст, с указанием мес€ца: ";
 	cin >> age;
 	cout << "введите номер курса: ";
 	cin >> course;
-	cout << "\nваши данные:\nфамили€: " << surname << " \nвозраст: " << age << " \nномер курса: " << course << "\n";*/
+	cout << "\nваши данные:\nфамили€: " << surname << " \nвозраст с указанием мес€ца: " << age << " \nномер курса: " << course << "\n";*/
 
 	//lab2.1
+
+
+	/*int num = 1;
+	int num1 = 10;
+	int num2 = 100;
+	int num3 = 20;
+	int num4 = 200;
+	int num5 = 2;
+	int num6 = 300;
+	int num7 = 3;
+	int num8 = 30;
+
+	cout << "" << num << "\t" "" << num1 << "\t" "" << num2 << "\t\n";
+	cout << "" << num3 << "\t" "" << num4 << "\t" "" << num5 << "\t\n";
+	cout << "" << num6 << "\t" "" << num7 << "\t" "" << num8 << "\t\n";*/
+
+
+	// lab3.1
+
+
+	//int b, c, d, f, j;
+	//cin >> b >> c >> d >> f >> j;
+
+	//int result = b + c / d - (f + j); // c / d дает в результате 1, поэтому int(целочисленный тип данных) преобразует в -18
+	//cout << result;
+
+	
+	// lab3.2
+
+
+	//float b, c, d, f, j;
+	//cin >> b >> c >> d >> f >> j;
+
+	//int result = b + c / d - (f + j); // с / d дает в результате 1.375 (из-за float), int преобразует 1.375 в 1, поэтому ответ -17
+	//cout << result;
+	
+
+	// lab3.3
+
+
+	//float b, c, d, f, j;
+	//cin >> b >> c >> d >> f >> j;
+
+	//float result = b + c / d - (f + j); // c / d дает в результате 1.375, поэтому float оставл€ет число с плавающей точкой, ответ -17
+	//cout << result;
+
+	
 
 	return 0;
 
